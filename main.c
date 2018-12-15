d01f2dc7a98d79d1eee192215a12a7dbc2158cdd
steven0129 <h3584935@gmail.com>
2018-12-16 00:12:47 +0800

    Move string comparison into function 'unistrcmp'

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 #include<string.h>
 #include"unicmp.h"
 
+int unistrcmp(char*, char*);
+
 int main(int argc, char *argv[]) {
     if (!setlocale(LC_ALL, "zh_TW.UTF-8")) {
         fprintf(stderr, "Error:Please check LANG, LC_CTYPE, LC_ALL.\n");
@@ -14,35 +16,39 @@ int main(int argc, char *argv[]) {
     char* pattern = "創造";
     char line[1000];
     while(fgets(line, sizeof(line), file)) {
-        for(int i = 0; i < strlen(line) - 1;) {
-            for(int j = 0; j < strlen(pattern) - 1;) {
-                if(is_ascii(line[i]) && !is_ascii(pattern[j])) {
-                    i++;
-                    j+=3;
-                } else if(!is_ascii(line[i]) && is_ascii(pattern[j])) {
-                    i+=3;
-                    j++;
-                } else if(is_ascii(line[i]) && is_ascii(pattern[j])) {
-                    if(asciichrcmp(line[i], pattern[j])) {
-                        printf("%s", line);
-                    }
+        unistrcmp(line, pattern);
+    }
+}
 
-                    i++;
-                    j++;
-                } else if(!is_ascii(line[i]) && !is_ascii(pattern[j])) {
-                    char t[3], p[3];
-                    for(int k=0; k<3; k++) {
-                        t[k] = line[i+k];
-                        p[k] = pattern[j+k];
-                    }
-                    
-                    if(unichrcmp(t, p)) {
-                        printf("%s", line);
-                    }
-
-                    i+=3;
-                    j+=3;
+int unistrcmp(char* text, char* pattern) {
+    for(int i = 0; i < strlen(text) - 1;) {
+        for(int j = 0; j < strlen(pattern) - 1;) {
+            if(is_ascii(text[i]) && !is_ascii(pattern[j])) {
+                i++;
+                j+=3;
+            } else if(!is_ascii(text[i]) && is_ascii(pattern[j])) {
+                i+=3;
+                j++;
+            } else if(is_ascii(text[i]) && is_ascii(pattern[j])) {
+                if(asciichrcmp(text[i], pattern[j])) {
+                    printf("%s", text);
                 }
+
+                i++;
+                j++;
+            } else if(!is_ascii(text[i]) && !is_ascii(pattern[j])) {
+                char t[3], p[3];
+                for(int k=0; k<3; k++) {
+                    t[k] = text[i+k];
+                    p[k] = pattern[j+k];
+                }
+                    
+                if(unichrcmp(t, p)) {
+                    printf("%s", text);
+                }
+
+                i+=3;
+                j+=3;
             }
         }
     }
