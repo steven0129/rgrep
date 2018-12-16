5393d6d85705e7c710a838f436a5ecafc2a83c48
steven0129 <h3584935@gmail.com>
2018-12-16 19:22:09 +0800

    Search engine is completed

--- main.c
+++ main.c
@@ -4,7 +4,8 @@
 #include<string.h>
 #include"unicmp.h"
 
-int unistrcmp(char*, char*);
+int min(int, int);
+int unistrcmp(char*, char*, int);
 
 int main(int argc, char *argv[]) {
     if (!setlocale(LC_ALL, "zh_TW.UTF-8")) {
@@ -13,43 +14,60 @@ int main(int argc, char *argv[]) {
     }
 
     FILE* file = fopen("bible.tsv", "r");
-    char* pattern = "創造";
+    char* pattern = "願主耶穌的恩惠";
     char line[1000];
     while(fgets(line, sizeof(line), file)) {
-        unistrcmp(line, pattern);
+        int err = unistrcmp(line, pattern, 2);
+        if(err != -1) printf("%d    %s", err, line);
     }
 }
 
-int unistrcmp(char* text, char* pattern) {
-    for(int i = 0; i < strlen(text) - 1;) {
-        for(int j = 0; j < strlen(pattern) - 1;) {
-            if(is_ascii(text[i]) && !is_ascii(pattern[j])) {
-                i++;
-                j+=3;
-            } else if(!is_ascii(text[i]) && is_ascii(pattern[j])) {
-                i+=3;
-                j++;
-            } else if(is_ascii(text[i]) && is_ascii(pattern[j])) {
-                if(asciichrcmp(text[i], pattern[j])) {
-                    printf("%s", text);
-                }
+int unistrcmp(char* text, char* pattern, int k) {
+    int A[strlen(pattern)], B[strlen(pattern)];
 
-                i++;
+    for(int i = 0; i < strlen(pattern); i++) B[i] = i;
+
+    for(int i = 0; i < strlen(text) - 1;) {
+        int jj = 1;
+
+        for(int j = 0; j < strlen(pattern) - 1;) {
+            A[0] = 0;
+            int error = 0;
+            if(is_ascii(text[i]) && !is_ascii(pattern[j])) {
+                j+=3;
+                error = 1;
+            } else if(!is_ascii(text[i]) && is_ascii(pattern[j])) {
+                j++;
+                error = 1;
+            } else if(is_ascii(text[i]) && is_ascii(pattern[j])) {
+                error = !asciichrcmp(text[i], pattern[j]);
                 j++;
             } else if(!is_ascii(text[i]) && !is_ascii(pattern[j])) {
                 char t[3], p[3];
                 for(int k=0; k<3; k++) {
                     t[k] = text[i+k];
                     p[k] = pattern[j+k];
-                }
                     
-                if(unichrcmp(t, p)) {
-                    printf("%s", text);
                 }
 
-                i+=3;
+                error = !unichrcmp(t, p);
                 j+=3;
             }
+
+            A[jj] = min( min(A[jj-1] + 1, B[jj] + 1) , B[jj-1] + error );
+            jj++;
         }
+
+        if(A[jj - 1] <= k) return A[jj - 1];
+        for(int j=0; j < jj; j++) B[j] = A[j];
+        
+        if(is_ascii(text[i])) i++;
+        else i+=3;
     }
+
+    return -1;
+}
+
+int min(int a, int b) {
+    return a<b ? a : b;
 }
